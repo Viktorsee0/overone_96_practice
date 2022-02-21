ac34bb7704c6f8b8d977a88dcdf449f6e2c53935
viktorsin <viktorsee0@gmail.com>
2022-02-21 21:53:08 +0300

    task reworked!

--- src/main/java/com.practice/controller/SuccessfulController.java
+++ src/main/java/com.practice/controller/SuccessfulController.java
@@ -3,16 +3,23 @@ package com.practice.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/successful_registration")
-public class SuccessfulRegistrationController {
+public class SuccessfulController {
+
+    private static final String AUTHORIZATION_MESSAGE = "Successful Authorization";
     private static final String REGISTER_MESSAGE = "Successful Registration";
 
-    @GetMapping
+    @GetMapping("/successful_authorization")
+    public String showSuccessfulAuthorization(Model model){
+        model.addAttribute("message", AUTHORIZATION_MESSAGE);
+        return "page";
+    }
+
+    @GetMapping("/successful_registration")
     public String showSuccessfulRegistrationPage(Model model){
         model.addAttribute("message", REGISTER_MESSAGE);
         return "page";
     }
+
 }
